b9bfe359996286f6cbd2e9d7aeac1ae7e6dc309b
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-06-15 20:58:41 +0100

    added buttons, tabs, eager loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 // Material section
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,6 +23,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatMenuModule,
     MatIconModule,
     MatSidenavModule,
     AppRoutingModule,
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
 
+// Material modules
+import { MatTabsModule } from '@angular/material/tabs';
+
+
 @NgModule({
   declarations: [OrderListComponent],
   imports: [
     CommonModule,
+    MatTabsModule,
     OrdersRoutingModule
   ]
 })
