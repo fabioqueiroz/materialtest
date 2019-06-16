acbcff7d78b3d33d6bd4aa581af1e834d13243c8
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-06-16 23:45:19 +0100

    date picker

---
+++ src/app/orders/order-new/order-new.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-order-new',
+  templateUrl: './order-new.component.html',
+  styleUrls: ['./order-new.component.css']
+})
+export class OrderNewComponent implements OnInit {
+
+  minDate = new Date();
+  maxDate = new Date(2019, 11, 31);
+  dateCtrl: FormControl;
+
+  constructor() { }
+
+  ngOnInit() 
+  {
+    this.dateCtrl = new FormControl('', [Validators.required]);
+  }
+
+}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,17 +3,38 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrderNewComponent } from './order-new/order-new.component';
 
 // Material modules
 import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
 
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderNewComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatMomentDateModule,
     OrdersRoutingModule
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
   ]
 })
 export class OrdersModule { }
