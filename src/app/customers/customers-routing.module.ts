2ece60535ee0f2bb2af5191ebd2f1b5a5521fbee
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-06-16 12:50:42 +0100

    customer card

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
 const routes: Routes = 
 [
-  { path: '', component: CustomerListComponent}
+  { path: '', component: CustomerListComponent},
+  { path: 'info', component: CustomerCardComponent}
 ];
 
 @NgModule({
